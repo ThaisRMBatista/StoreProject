fcdac8073f8c5ef63b56111924fe5c18251c8225
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-22 19:35:20 -0300

    feat: refactor

---
+++ src/br/com/pattern/store/order/action/SaveOrderDB.java
@@ -0,0 +1,10 @@
+package br.com.pattern.store.order.action;
+
+import br.com.pattern.store.order.Order;
+
+public class SaveOrderDB {
+
+    public void execute(Order order) {
+        System.out.println("Save order in dataBase");
+    }
+}
